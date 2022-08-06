ce02f6c346056ad5d61deae9c1a00ec1911649a5
Yassine Zemmouri <yzemmouri.eng@gmail.com>
2022-08-06 10:07:41 -0400

    parallax and fixed top bar

--- js/logic.js
+++ js/logic.js
@@ -1,9 +1,13 @@
 "use strict";
 
-const parallax = document.getElementById("hero");
+const heroEl = document.getElementById("hero");
 
+//parallax function
 window.addEventListener("scroll", function(){
     let offset = window.pageYOffset;
-    parallax.style.backgroundPositionY=offset * 0.9 + "px";
+    heroEl.style.backgroundPositionY=offset * 0.5 + "px";
+    console.log('offcet: '+offset);
+    console.log('position: '+offset*0.5);
+
 
 })
